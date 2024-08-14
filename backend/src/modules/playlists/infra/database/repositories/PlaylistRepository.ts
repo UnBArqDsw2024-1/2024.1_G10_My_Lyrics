0258b7827458135d6310489d41243de8891b1b6c
Gustavo Kenzo Araki Takechi <gustavokenzo314@gmail.com>
2024-08-14 17:07:22 -0300

    inclui likes

--- backend/src/modules/playlists/infra/database/repositories/PlaylistRepository.ts
+++ backend/src/modules/playlists/infra/database/repositories/PlaylistRepository.ts
@@ -3,86 +3,126 @@ import { DatabaseConnection } from "../../../../../infra/database/GetConnection"
 import type { IPlaylistRepository } from "../../../repositories/IPlaylistRepository";
 
 export class PlaylistRepository implements IPlaylistRepository {
-  private prismaClient: PrismaClient;
+	private prismaClient: PrismaClient;
 
-  private constructor() {
-    this.prismaClient = DatabaseConnection.getInstance();
-  }
+	private constructor() {
+		this.prismaClient = DatabaseConnection.getInstance();
+	}
 
-  private static INSTANCE: PlaylistRepository | null;
-  public static getInstance() {
-    if (!PlaylistRepository.INSTANCE) {
-      PlaylistRepository.INSTANCE = new PlaylistRepository();
-    }
+	private static INSTANCE: PlaylistRepository | null;
 
-    return PlaylistRepository.INSTANCE;
-  }
+	public static getInstance() {
+		if (!PlaylistRepository.INSTANCE) {
+			PlaylistRepository.INSTANCE = new PlaylistRepository();
+		}
 
-  async create(playlist: Prisma.PlaylistCreateInput): Promise<Playlist> {
-    return this.prismaClient.playlist.create({ data: playlist });
-  }
+		return PlaylistRepository.INSTANCE;
+	}
 
-  async searchByTitle(name: string): Promise<Playlist[]> {
-    return this.prismaClient.playlist.findMany({
-      where: {
-        title: {
-          contains: name,
-          mode: "insensitive",
-        },
-      },
-    });
-  }
+	async create(playlist: Prisma.PlaylistCreateInput): Promise<Playlist> {
+		return await this.prismaClient.playlist.create({ data: playlist });
+	}
 
-  async deleteById(id: string): Promise<void> {
-    await this.prismaClient.playlist.delete({
-      where: {
-        id,
-      },
-    });
-  }
+	async searchByTitle(name: string): Promise<Playlist[]> {
+		return await this.prismaClient.playlist.findMany({
+			where: {
+				title: {
+					contains: name,
+					mode: "insensitive",
+				},
+			},
+			include: {
+				_count: {
+					select: {
+						likes: true,
+					},
+				},
+			},
+		});
+	}
 
-  async getById(id: string): Promise<(Playlist & { musics: Music[] }) | null> {
-    const playlist = await this.prismaClient.playlist.findUnique({
-      where: {
-        id,
-      },
-      include: {
-        musics: true,
-      },
-    });
+	async deleteById(id: string): Promise<void> {
+		await this.prismaClient.playlist.delete({
+			where: {
+				id,
+			},
+		});
+	}
 
-    return playlist;
-  }
+	async getById(
+		id: string,
+	): Promise<(Playlist & { musics: Music[]; likes: number }) | null> {
+		const playlist = await this.prismaClient.playlist.findUnique({
+			where: {
+				id,
+			},
+			include: {
+				musics: true,
+				_count: {
+					select: {
+						likes: true,
+					},
+				},
+			},
+		});
 
-  async addMusic(playlistId: string, musicId: string): Promise<(Playlist & { musics: Music[] }) | null> {
-    const playlist = await this.prismaClient.playlist.update({
-      where: {
-        id: playlistId,
-      },
-      data: {
-        musics: {
-          connect: {
-            id: musicId,
-          },
-        },
-      },
-      include: {
-        musics: true,
-      },
-    });
+		if (!playlist) {
+			return null;
+		}
 
-    return playlist;
-  }
+		const {
+			_count: { likes },
+			...remaining
+		} = playlist;
 
-  
-  async likePlaylist(playlistId: string, user_Id: string): Promise<void> {
-    await this.prismaClient.playlist.update({
-      where: { id: playlistId },
-      data: {
-        likes: {
-          connect: { id: user_Id },
-        },
-      },
-    });
-  }
+		return { ...remaining, likes };
+	}
+
+	async addMusic(
+		playlistId: string,
+		musicId: string,
+	): Promise<(Playlist & { musics: Music[]; likes: number }) | null> {
+		const playlist = await this.prismaClient.playlist.update({
+			where: {
+				id: playlistId,
+			},
+			data: {
+				musics: {
+					connect: {
+						id: musicId,
+					},
+				},
+			},
+			include: {
+				musics: true,
+				_count: {
+					select: {
+						likes: true,
+					},
+				},
+			},
+		});
+
+		if (!playlist) {
+			return null;
+		}
+
+		const {
+			_count: { likes },
+			...remaining
+		} = playlist;
+
+		return { ...remaining, likes };
+	}
+
+	async likePlaylist(playlistId: string, user_Id: string): Promise<void> {
+		await this.prismaClient.playlist.update({
+			where: { id: playlistId },
+			data: {
+				likes: {
+					connect: { id: user_Id },
+				},
+			},
+		});
+	}
 }
