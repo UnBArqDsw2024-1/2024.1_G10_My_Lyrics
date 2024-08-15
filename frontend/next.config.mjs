32f0190845089b4d37b8b3b40208c80aa320e53d
Gustavo Kenzo Araki Takechi <gustavokenzo314@gmail.com>
2024-08-15 15:27:05 -0300

    tela de letra

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["i.ytimg.com", "my-lyrics.learnwithpadas.com", "localhost"],
+    domains: ["i.ytimg.com", "my-lyrics.learnwithpadas.com", "akamai.sscdn.co"],
   },
 };
 
--- frontend/src/lib/types/data.ts
+++ frontend/src/lib/types/data.ts
@@ -1,47 +1,48 @@
 // essas interfaces foram feitas sem muito cuidado, alterem de acordo com o que for necessário
 
 export interface Playlist {
-  id: string
-  userId: string
-  title: string
-  description: string
-  musics: Music[]
+	id: string;
+	userId: string;
+	title: string;
+	description: string;
+	musics: Music[];
 }
 
 export interface Music {
-  id: string
-  albumId: string
-  artists: Artists[]
-  title: string
-  youtubeUrl: string
-  iconUrl: string
-  album: Album
-  verses: Verse[]
-  playlists: Playlist[]
-  likes: User[]
-  musicAccess: MusicAccess[]
+	id: string;
+	albumId: string;
+	artists: Artists[];
+	title: string;
+	youtubeUrl: string;
+	iconUrl: string;
+	album: Album;
+	verses: Verse[];
+	playlists: Playlist[];
+	likes: User[];
+	musicAccess: MusicAccess[];
 }
 
 export interface Artists {
-  id: string
-  name: string
-  biography: string
+	id: string;
+	name: string;
+	biography: string;
+	albums: Album[];
 }
 
 export interface Album {
-  id: string
-  artists: Artists[]
-  title: string
-  musics: Music[]
+	id: string;
+	artists: Artists[];
+	title: string;
+	musics: Music[];
 }
 
 export interface Verse {
-  id: string
-  startTime: number
-  endTime: number
-  text: string
-  translatedText: string
-  music: Music
+	id: string;
+	startTime: number;
+	endTime: number;
+	text: string;
+	translatedText: string;
+	music: Music;
 }
 
 export interface User {
@@ -54,9 +55,8 @@ export interface User {
   playlists: Playlist[]
 }
 export interface MusicAccess {
-  ip: string
-  musicId: string
-  date: Date
-  music: Music
+	ip: string;
+	musicId: string;
+	date: Date;
+	music: Music;
 }
-
