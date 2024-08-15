15e6a185d85842e1be9644a2cd6c38669088582c
Gustavo <gustavohenriqueprivado@gmail.com>
2024-08-15 16:49:47 -0300

    Coisa demais Co-authored-by: brunomed <bruno.med.oliv@gmail.com>

--- frontend/src/app/playlist/page.tsx
+++ frontend/src/app/playlist/page.tsx
@@ -1,12 +1,29 @@
 "use client";
 
+import PlaylistCard from "@/components/PlaylistCard";
 import { UserContext } from "@/context/UserContext";
-import { useContext } from "react";
+import { redirect } from "next/navigation";
+import { use } from "react";
 
 export default function Playlist() {
-  const user = useContext(UserContext);
+  const user = use(UserContext);
 
-  if (user.user === false) {
-    return <div>Login</div>;
+  if (!user.user) {
+    redirect("/");
   }
+
+  const playlistMusics = user.user.playlists.flatMap(
+    (playlist) => playlist.musics
+  );
+
+  return (
+    <div className="flex flex-col items-center justify-center text-white mt-40 gap-10 w-screen">
+      <h1 className="text-3xl font-bold">Playlist</h1>
+      <div className="flex items-center w-full gap-4 flex-col">
+        {user.user.playlists?.map((playlist) => (
+          <PlaylistCard key={playlist.id} playlist={playlist} />
+        ))}
+      </div>
+    </div>
+  );
 }
---
+++ frontend/src/components/Modal.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import { api } from "@/lib/api";
+import { Playlist, Music } from "@/lib/types/data";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  playlist: Playlist;
+}
+
+export default function Modal({ isOpen, onClose, playlist }: ModalProps) {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredMusic, setFilteredMusic] = useState<Music[]>([]);
+
+  async function handleSearch(e: React.ChangeEvent<HTMLInputElement>) {
+    const query = e.target.value;
+    setSearchTerm(query);
+
+    if (query.length > 2) {
+      try {
+        const res = await api.get(`/music/search?title=${query}`);
+        setFilteredMusic(res.data);
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      setFilteredMusic([]);
+    }
+  }
+
+  async function handleAddMusic(musicId: string) {
+    try {
+      const res = await api.post(`/playlist/${playlist.id}/music`, {
+        musicId,
+      });
+      console.log(res);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    isOpen && (
+      <div className="fixed inset-0 z-50 overflow-y-auto bg-black/50 backdrop-blur-sm transition-opacity duration-300 flex items-center justify-center">
+        <div className="flex min-h-screen items-center justify-center p-4 text-center w-2/4">
+          <div className="relative bg-[#3F3352] rounded-lg shadow-lg">
+            <div className="p-6">
+              <div className="flex items-center justify-between">
+                <h3 className="text-xl font-bold text-white text-left">
+                  A sua playlist {playlist.title} está pronta para receber uma
+                  musica nova!
+                </h3>
+                <button
+                  type="button"
+                  className="text-gray-400 hover:text-gray-500"
+                  onClick={onClose}
+                >
+                  <FontAwesomeIcon icon={faTimes} className="w-5 h-5" />
+                </button>
+              </div>
+              <div className="mt-5 sm:mt-6">
+                <form className="w-full flex flex-col gap-8">
+                  <input
+                    type="text"
+                    name="musicId"
+                    placeholder="Nome da música"
+                    value={searchTerm}
+                    onChange={handleSearch}
+                    className="w-full px-4 py-3 bg-[#b8b2ca] bg-opacity-20 rounded-xl focus:outline-none focus:ring-0 backdrop-blur-md font-roboto text-white"
+                  />
+                  {filteredMusic.length > 0 && (
+                    <ul className="bg-[#3F3352] rounded-lg max-h-40 overflow-y-auto mt-2">
+                      {filteredMusic.map((music) => (
+                        <li
+                          key={music.id}
+                          onClick={() => handleAddMusic(music.id)}
+                          className="cursor-pointer hover:bg-[#564a66] px-4 py-2"
+                        >
+                          {music.title}
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                  <div className="flex gap-4">
+                    <button
+                      onClick={onClose}
+                      type="button"
+                      className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-[#d06161] text-base font-medium hover:bg-[#a34949] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-1 sm:text-sm text-white"
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      type="button"
+                      className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-[#6BC5D2] text-base font-medium text-white hover:bg-[#417a83] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-2 sm:text-sm"
+                    >
+                      Save
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  );
+}
